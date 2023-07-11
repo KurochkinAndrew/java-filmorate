b68e92d4a967f550a15f8cdfbb4e366d05611014
Курочкин Андрей <andrylol228@mail.ru>
2023-07-12 03:53:34 +1000

    ТЗ 11

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -26,7 +26,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-class FilmoRateApplicationTests {
+class FilmorateApplicationTests {
     final FilmDbStorage filmDbStorage;
     final UserDbStorage userDbStorage;
     final LikesStorage likesStorage;
